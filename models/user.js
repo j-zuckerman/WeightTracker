11df7441db27c082ae6e09979108109eb0f372fb
James Zuckerman <jzuckerman4@gmail.com>
2019-08-27 19:51:29 -0700

    Setting up API

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+//in order to make sure username is unique
+const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = mongoose.Schema({
+  username: { type: String, unique: true },
+  name: String,
+  passwordHash: String,
+  weightMeasurements: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Weight'
+    }
+  ]
+});
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
